a9075069af84546551fce7f2dcf34940a8cdad94
xAYAAMIR <xAMIRAYA>
2024-12-26 14:50:57 +0100

    aya

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/aya.jpg";
+import myImg from "../../Assets/aya2.jpg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
