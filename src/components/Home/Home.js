3534e5b674b7a3d80c7a17293b1eb34345c075ca
xAYAAMIR <xAMIRAYA>
2024-12-26 15:14:37 +0100

    aya

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/aya3.jpg";
+import homeLogo from "../../Assets/aya3.png";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
